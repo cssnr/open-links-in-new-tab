7a17316bba18eece7ee5b0e151150a1df14cc540
Shane <6071159+smashedr@users.noreply.github.com>
2023-11-18 13:59:55 -0800

    Cleanup

--- src/js/main.js
+++ src/js/main.js
@@ -2,12 +2,12 @@
 
 document.querySelectorAll('[data-locale]').forEach((el) => {
     if (el.dataset.locale?.toString() === 'version') {
-        el.innerText = chrome.runtime.getManifest().version
+        el.textContent = chrome.runtime.getManifest().version
     }
     // } else if (el.dataset.locale) {
     //     const value = chrome.i18n.getMessage(el.dataset.locale)
     //     if (value) {
-    //         el.innerText = value
+    //         el.textContent = value
     //     } else {
     //         console.warn(`Value Not Found for ${el.dataset.locale}`)
     //     }
